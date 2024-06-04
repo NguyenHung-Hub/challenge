80c52f2e9fabdaece937dfed813b7172b9ab8898
Hung <daminhnguyenhung@gmail.com>
2024-06-04 22:44:39 +0700

    add plugin format tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import Card from "./component/Card";
 import data from "./data";
 
 function App() {
-    return (
-        <>
-            <div className="w-full min-h-screen bg-slate-800 flex items-center justify-center">
-                <div className="my-16 grid grid-cols-1 gap-16">
-                    {data.map((i) => (
-                        <Card key={i.id} props={i} />
-                    ))}
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex min-h-screen w-full items-center justify-center bg-slate-800">
+        <div className="my-16 grid grid-cols-1 gap-16">
+          {data.map((i) => (
+            <Card key={i.id} props={i} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
